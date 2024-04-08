b377bddfcb41935432a6c220e6290c5adf18e645
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-08 13:53:13 +0530

    init header component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '../../assets/images/logo.png'
 
 export default function Header() {
-    return <header>
+    return <header id='header'>
          <img src={Logo} alt='logo' />
          <h1>Investment Calculator</h1>
     </header>
