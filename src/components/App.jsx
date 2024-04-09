a5c21bb4f412b3b48afe87c8e4e3956fdbe00cf2
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-10 00:32:34 +0530

    lifting the stateup to app component from input component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
+
 import Header from './Layout/Header.jsx'
 import Main from './Layout/Main.jsx'
 import Section from './Common/Section.jsx'
 import UList from './Common/UList.jsx'
 import Input from './Common/Input.jsx'
+import Results from './Results/Results.jsx'
 
 const App = () => {
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier] : newValue
+            }
+        })
+    }
+
     return (
         <>
             <Header/>
             <Main>
                 <Section id='user-input'>
                     <UList className='input-group'>
-                        <Input label='Initial Investment' identifier='initialInvestment' type='number' required />
-                        <Input label='Annual Investment' identifier='annualInvestment' type='number' required />
+                        <Input userInput={userInput} label='Initial Investment' onChange={handleChange} identifier='initialInvestment' type='number' required />
+                        <Input userInput={userInput} label='Annual Investment' onChange={handleChange} identifier='annualInvestment' type='number' required />
                     </UList>
 
                     <UList className='input-group'>
-                        <Input label='Expected Return' identifier='expectedReturn' type='number' required />
-                        <Input label='Duration' identifier='duration' type='number' required />
+                        <Input userInput={userInput} label='Expected Return' onChange={handleChange} identifier='expectedReturn' type='number' required />
+                        <Input userInput={userInput} label='Duration' onChange={handleChange} identifier='duration' type='number' required />
                     </UList>
+                </Section>
+                <Section>
+                    <Results input={userInput}/>
                 </Section>
             </Main>
         </>
--- src/components/Common/Input.jsx
+++ src/components/Common/Input.jsx
@@ -1,27 +1,8 @@
-import { useState } from 'react'
-
-export default function Input({ label, identifier, ...props}) {
-
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    })
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier] : newValue
-            }
-        })
-    }
-
+export default function Input({ label, identifier, onChange, userInput, ...props}) {
     return <li>
         <label>{label}</label>
         <input {...props}
          value={userInput[identifier]}
-         onChange={(event) => handleChange(identifier, event.target.value)}/>
+         onChange={(event) => onChange(identifier, event.target.value)}/>
     </li>
 }
